df1a68f26d8621ffb97842b1041a23e532e0672f
Tim5PPLG4 <144770920+Tim5PPLG4@users.noreply.github.com>
2023-09-27 22:59:15 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,10 @@
+<?php 
+ // mengaktifkan session php
+session_start();
 
+// menghapus semua session
+session_destroy();
+
+// memgalihkan halaman ke halaman login
+header("location:index.php");
+?>
